3b5f0ce97b52b3f4665e90e7403601aa0e7a6c0b
lukepow <lukespowers@gmail.com>
2024-06-25 16:49:32 -0700

    working sales report gen

--- lambda_scripts/ads/script.js
+++ lambda_scripts/ads/script.js
@@ -71,10 +71,9 @@ const reportInfo = {
 
 async function saveReport(profileId, reportInfo, fileName) {
   const accessToken = await getAccessToken();
-  // const response = await requestReport(apiUrl, profileId, accessToken, yesterday, reportInfo);
-  // const reportId = response.reportId;
-  // const reportUrl = await getReportUrl(accessToken, profileId, reportId);
-  const reportUrl = "https://offline-report-storage-us-east-1-prod.s3.amazonaws.com/4e592d92-0705-40d6-ac2c-49f6663b1865-1719254327614/report-4e592d92-0705-40d6-ac2c-49f6663b1865-1719254327614.json.gz?X-Amz-Security-Token=IQoJb3JpZ2luX2VjEAMaCXVzLWVhc3QtMSJIMEYCIQDHlIV4rx5De5%2FKOxrdeFBqQqTkZdfP6%2FexGsyoza3jagIhAIuRnVfdeOdcqWcIbROkxVZCCHsizAzFvWLjloXoHGDmKuoFCKz%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEQAhoMODQxNjQ1NTQzMDc0IgzQuc%2FCitmeZNqIdawqvgUV3SPvTz0Ik9mLK9uAHFozZhcdTXvLLi%2BhxqeLl816LWvgrHEExRNPvN50vwk5WuoSx7w%2FRcDWPbi95IVNX6OYgIGCZrTMbA4tSDUCkcii%2B%2F0rl6%2B5gqxHJnBV1FrMuVejOxKz4spbE%2B4Uz1yC1KE6mhFsvg4Lq%2FGdjC2xXOkZwBBO%2B5P1p%2FimKM92EYdO1qwtrK4o%2B1%2FVqIYtEh1XBLu96jqi2qEh1SowaZCT7msDNfqjjrNG7Aov7vvNFLYp0wgVrlfxXYtaNFhdLwi9j2lF8dFycQfUrQ9UhVY6wjDGRkjP55KJNoW5aj9R7knsG425bsyCbQL9dJ28t%2BUuhR5n7PDxoMfPZygr0SbC7teT%2Fj2ctSkMwc6idSkrPgrsedhyN%2BkOF8Kpl6p2we0RXvs050u03PfjuTfIZusSKAiAIRIAvos%2FEQYqZbHkQU15OMRlUax2%2BtDbywOq8hXIIpN7BVCE8z1KiZq%2ByhHHYfkTBtHZP1jqLlynjgvQokgScia36mBiuWCRpK0ZdhH2lUlWYn5UHBixrPteHJgSPSltgtnNXy6pi7qDQEaC2bbE9A3SX%2F4di9ajaIZblnJVGkvfvm0RDMbWT%2B3ARSdSR%2Frspsjq0AMwH9wUN8KvIeAkoHEvgME7OJiXox%2BloPFB8XiIS5YANTKjyBQlc49jkd53CdMbye4n4BgZnm3TWnV771gr2ZTAz%2FoGFX65dKIPF297l4ZPGZzmuEZcjkwZ5ZIpegJD1PBhGxIU6lh1UVUGg3CLnlAXlH%2BXLieJOLHgdkHszfMZ6OMuz6wZ8rBN%2FUEk%2BtUkW9th%2FHsTXX1V6CVUSwDIeBgio6HpgFC78DcXtOIWca8ZcxK4AVsxFxKeAf38ZdPRzfpe6Tk8HRDp0giHHCMoLl78r1acIH%2FynRTsXgQyUdhVvNBL0bOY%2Fw60cd0wh%2FXmswY6qAGtHAFjkwcDfnEBGPQbg%2FgowuLNubetGQtM%2BK9%2BHs272s0DsuVR%2B370m5PC1rIpuZpS6Jdv6sf7GuopbSBtwzqdMr%2FXgVwO%2FPcfmEyevrQr92Sg63fP0zoIspeWgcornMH35uGxswRWfKvAFi1Y80ZGAhve5NccXvMtyTIexBxaxv27PdjbC6QaUjUQtwm0b7yUKMeSvP41UzbbwVsYmvphnwRzneUggSw%3D&X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Date=20240624T184821Z&X-Amz-SignedHeaders=host&X-Amz-Expires=3600&X-Amz-Credential=ASIA4H5P3Z2RO45QKVYO%2F20240624%2Fus-east-1%2Fs3%2Faws4_request&X-Amz-Signature=da08bace841dfd843b4e63c3288c82bdd3d041ade76905bd1cc90ad0c7d34536"
+  const response = await requestReport(apiUrl, profileId, accessToken, yesterday, reportInfo);
+  const reportId = response.reportId;
+  const reportUrl = await getReportUrl(accessToken, profileId, reportId);
   const reportData = await getReportData(reportUrl);
   const cleanedData = reorderColumns(reportData, reportInfo.columns);
 
--- lambda_scripts/sales/script.js
+++ lambda_scripts/sales/script.js
@@ -1,3 +1,81 @@
+import axios from "axios";
+import dotenv from "dotenv";
+import path from "path";
+import fs from "fs";
+import { SellingPartner } from "amazon-sp-api";
+dotenv.config();
+
+const tokenUrl = "https://api.amazon.com/auth/o2/token";
+const apiUrl = "https://sellingpartnerapi-na.amazon.com/orders/v0/orders";
+const clientId = process.env.CLIENT_ID;
+const clientSecret = process.env.CLIENT_SECRET;
+const refreshToken = process.env.REFRESH_TOKEN;
+const yesterday = new Date();
+const today = new Date();
+yesterday.setDate(yesterday.getDate()-1);
+today.setDate(today.getDate());
+
+const dateVals = yesterday.toISOString().split("T")[0].split("-");
+const startDate = `${yesterday.toISOString().split("T")[0]}T07:00:00+00:00`;
+const endDate = `${today.toISOString().split("T")[0]}T07:00:00+00:00`;
+const yesterdayUsaFormat = `${dateVals[1]}-${dateVals[2]}-${dateVals[0]}`;
 
 
-const apiUrl = "https://sellingpartnerapi-na.amazon.com";
+
+const marketplaceIds = {USA: "ATVPDKIKX0DER", CA: "A2EUQ1WTGCTBG2", MX: "A1AM78C64UM0Y8"};
+
+const spClient = new SellingPartner({
+  region: "na", // north america
+  refresh_token: refreshToken
+});
+
+async function fetchData() {
+  const filePath = path.join("/Users/lukepowers/Downloads", `${yesterdayUsaFormat}test.txt`);
+  try {
+    let res = await spClient.downloadReport({
+      body: {
+        reportType: "GET_FLAT_FILE_ALL_ORDERS_DATA_BY_ORDER_DATE_GENERAL",
+        marketplaceIds: ["ATVPDKIKX0DER"],
+        dataStartTime: startDate,
+        dataEndTime: endDate
+      },
+      version: "2021-06-30",
+      interval: 8000,
+      
+    });
+    return res;
+    
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+function transformData(tsvData) {
+  const rows = tsvData.split("\n");
+  const alteredRows = [];
+  rows.forEach((row, index) => {
+    let columns = row.split("\t");
+    if (index === 0) {
+      columns.splice(8, 0, "url");
+    } else {
+      columns.splice(8, 0, " ");
+    }
+    
+    alteredRows.push(columns.join("\t"));
+  })
+  return alteredRows.join("\n");
+}
+
+function saveDocument(filePath, tsvData) {
+  try {
+    fs.writeFileSync(filePath, tsvData);
+  } catch(err) {
+    console.error('Error:', err.response ? err.response.data : err.message);
+  }
+}
+
+const data = await fetchData();
+const transformedData = transformData(data);
+const filePath = path.join("/Users/lukepowers/Downloads", `${yesterdayUsaFormat}testy.txt`);
+saveDocument(filePath, transformedData);
+
